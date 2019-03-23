e2d8aa41593ced289269b157f3af495b687cf549
john <vivelab@rutanmedellin.org>
2019-03-23 13:14:26 -0500

    Javascript
    Se agrega JS al proyecto

---
+++ scripts.js
@@ -0,0 +1,10 @@
+//WEB API
+
+const item1 = document.createElement('li') //<li></li>
+item1.textContent = "teaching on team international's topgun lab."
+
+//document.querySelector
+const lista = document.querySelector("#mi-lista")
+
+//Destino.appendchild(nodo -a-agregar
+lista.appendChild(item1)
