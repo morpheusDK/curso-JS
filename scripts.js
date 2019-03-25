69ddf7c4e74a2da1b7512a5595c05d87653fe907
morpheusDK <daniel1456@gmail.com>
2019-03-24 23:23:05 -0500

    Final de página
    Se agregan videos de reproducción

--- scripts.js
+++ scripts.js
@@ -1,10 +1,19 @@
-//WEB API
+let videos = Array.from(document.querySelectorAll('.videos'));
 
-const item1 = document.createElement('li') //<li></li>
-item1.textContent = "teaching on team international's topgun lab."
+document.querySelector('#boton-watch').addEventListener('click', e => {
+    let pos1 = 0;
+    for (let j = 0; j < videos.length; j++) {
+        if (videos[j].classList.contains('active')) {
+            pos1 = j;
+        }
+    }
+    videos.map(video => video.classList.remove('active'));;
+    if (pos1 == 2) {
+        pos1 = 0;
+        videos[pos1].classList.add('active');
+    } else {
+        pos1++;
+        videos[pos1].classList.add('active');
+    }
 
-//document.querySelector
-const lista = document.querySelector("#mi-lista")
-
-//Destino.appendchild(nodo -a-agregar
-lista.appendChild(item1)
+})
